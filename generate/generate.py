349c0efd3768e7932dcceaf2083a1eee85ab84a9
nit3owl <nit3owldev@gmail.com>
2015-10-14 23:41:28 -0700

    Cleanup files, use argparse, and begin implementing random generation

--- generate/generate.py
+++ generate/generate.py
@@ -1,20 +1,30 @@
-def generate_codes(num, size):
+def get_random_code(size):
+    return ''.join(random.choice(alphanumeric) for _ in range(size))
+
+def generate_codes(num, size, random):
     output = open('output.csv', 'w')
-    for i in range(0, num):
-        temp = "%0"+size+"d" 
-        temp = temp % (i,)
-        output.write("%s,\n" % temp) 
+    if not random:
+        for i in range(0, num):
+            temp = '%0'+str(size)+'d' 
+            temp = temp % (i,)
+            output.write('%s,\n' % temp)
+    else:
+        for i in range(0, num):
+            output.write('%s,\n' % get_random_code(size))
     output.close()
 
-def main(args):
-    try:
-        num  = int(args[1])
-        size = args[2]
-    except:
-        print "Usage: %s <numToGen> <lengthOfCodes>" % sys.argv[0]
-        sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(description='Generate a .csv file with N codes of Y length.')
+    parser.add_argument('num', metavar='N', type=int, help='number of codes to generate')
+    parser.add_argument('size', metavar='S', type=int, help='length of codes')
+    parser.add_argument('-r', metavar='--random', action='store_const', const=True, default=False, dest='random', required=False, 
+            help='request pseudo-random, unique (per exec) codes')
+    args = parser.parse_args()
+    generate_codes(args.num, args.size, args.random)
+    print "Generation complete, codes ouput in output.csv."
 
-    generate_codes(num, size)
-
-import sys
-main(sys.argv)
+import argparse
+import string
+import random
+alphanumeric = string.ascii_uppercase + string.digits
+main()
