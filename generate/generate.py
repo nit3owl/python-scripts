d0f24c381de6375c3ca515f5de2ea6b276e2a46b
nit3owl <nit3owldev@gmail.com>
2015-10-15 15:59:47 -0700

    Issue-2: Add checks on input to ensure unique codes

--- generate/generate.py
+++ generate/generate.py
@@ -20,11 +20,26 @@ def main():
     parser.add_argument('-r', metavar='--random', action='store_const', const=True, default=False, dest='random', required=False, 
             help='request pseudo-random, unique (per exec) codes')
     args = parser.parse_args()
+    if not args.random:
+        if args.num > (args.size * 10):
+            print  ("ERROR: Cannot generate unique codes becuase num codes requested must be " +
+                    "less than or equal to requsted code size x 10")
+            print  ("num was '" + str(args.num) + "' but max num for code size '" + str(args.size) + 
+                    "' is '" + str((args.size * 10)) + "'")
+            sys.exit(1)
+    else:
+        if args.num > (args.size * len(alphanumeric)):
+            print  ("ERROR: Cannot generate unique \"random\" codes becuase num codes requested must be " +
+                    "less than or equal to requsted code size x " + str(len(alphanumeric)))
+            print  ("num was '" + str(args.num) + "' but max num for code size '" + str(args.size) + 
+                    "' is '" + str((args.size * len(alphanumeric))) + "'")
+            sys.exit(1)
     generate_codes(args.num, args.size, args.random)
-    print "Generation complete, codes ouput in output.csv."
+    print ("Generation complete, codes ouput in output.csv.")
 
 import argparse
 import string
 import random
+import sys
 alphanumeric = string.ascii_uppercase + string.digits
 main()
