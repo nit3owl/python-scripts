f82e95cf36ef5fab5d91fedc3f842de937364763
nit3owl <nit3owldev@gmail.com>
2019-10-27 23:47:11 -0700

    Add filtering by author when aggregating commits by user

--- git-utils/git_activity.py
+++ git-utils/git_activity.py
@@ -9,8 +9,10 @@ import operator
 from collections import OrderedDict
 
 def print_output(output_map, sort):
+    if len(output_map) == 0:
+        print('No results.')
+
     if sort == True:
-        print("hit")
         output_map = OrderedDict(sorted(output_map.items(), key=lambda x: x[1], reverse=True))
 
     for key, value in output_map.items():
@@ -20,7 +22,7 @@ def check_and_pull():
     subprocess.call(['git', 'checkout', 'master'])
     subprocess.call(['git', 'pull'])
 
-def count_commits_per_month(repos):
+def count_commits_per_month(repos, author):
     commits_per_month = OrderedDict()
 
     for repo in repos:
@@ -63,7 +65,7 @@ def count_commits_per_month(repos):
 
     print_output(commits_per_month, False)
 
-def count_commits_per_user(repos):
+def count_commits_per_user(repos, author):
     commits_per_user = {}
     current_year = datetime.now().year
 
@@ -85,11 +87,12 @@ def count_commits_per_user(repos):
                 repo_commits = arr[0].lstrip()
                 user = arr[1]
 
-                if user in commits_per_user:
-                    commits = commits_per_user[user]
-                    commits_per_user[user] = commits + int(repo_commits)
-                else:
-                    commits_per_user[user] = int(repo_commits)
+                if (not author) or (author and author == user):
+                    if user in commits_per_user:
+                        commits = commits_per_user[user]
+                        commits_per_user[user] = commits + int(repo_commits)
+                    else:
+                        commits_per_user[user] = int(repo_commits)
         
         os.chdir(wd)
 
@@ -102,7 +105,7 @@ def main():
     parser = argparse.ArgumentParser(description = 'Use git\'s logging to count commits')
     parser.add_argument('metric', nargs='?', help='user = count commits by user | month = count commits by month')
     parser.add_argument('--dir', nargs='?', help='root directory to search for git repos - defaults to current')
-    #parser.add_argument('--author', nargs='?', help='counts commits only by the supplied author')
+    parser.add_argument('--author', nargs='?', help='counts commits only by the supplied author')
 
     args = parser.parse_args()
 
@@ -113,9 +116,12 @@ def main():
             directory = args.dir
         metric = args.metric
         if metric == 'user':
-            count_commits_per_user(get_directory_list(directory))
+            count_commits_per_user(get_directory_list(directory), args.author)
         elif metric == 'month':
-            count_commits_per_month(get_directory_list(directory))
+            # TODO impl and remove
+            if args.author is not None:
+                print("\"author\" is not currently supported when counting commits by user - ignoring.")
+            count_commits_per_month(get_directory_list(directory), args.author)
         else:
             parser.print_help(sys.stderr)
 
